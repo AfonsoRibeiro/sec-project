618f84638b5a8c8aa07bccae2bdbd20b5720316d
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2021-03-23 20:06:04 +0000

    started on gid creation

--- grid/src/main.rs
+++ grid/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+fn parse_args(args: &[String]) -> (u32, u32, u32) {
+    if args.len() != 4 {
+        println!("Error: Argument must be 3, [size of grid] [number of points] [number of ephocs]");
+        std::process::exit(1);
+    }
+
+    (args[1].parse().unwrap(), args[2].parse().unwrap(), args[3].parse().unwrap())
 }
+
+struct Grid {
+    grid : Vec<Vec<i32>>
+}
+
+// impl Grid {
+//     fn new(size : u32) -> Grid {
+//         { grid = vec}
+//     }
+// }
+
+struct Timeline {
+    timeline : Vec<Grid>
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let (size, points, ephocs) = parse_args(&args);
+
+
+}
+
+fn create_grid() {
+
+}
+
