227b34c7b5babc3bdf5b40e9647762bff8a7cc96
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2021-03-27 19:46:51 +0000

    started on the client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+
+use structopt::StructOpt;
+use color_eyre::eyre::Result;
+
+#[derive(StructOpt)]
+#[structopt(name = "Client", about = "Reporting and verifying locations since 99.")]
+struct Opt {
+    #[structopt(name = "server", long)]
+    server_url : String, // TODO
+    #[structopt(long)]
+    idx : usize,
+    #[structopt(name = "file", long)]
+    grid_file : String
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let opt = Opt::from_args();
+
+
+    Ok(())
+
 }
