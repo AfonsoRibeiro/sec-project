e2d988f6484bef4f549ef3ce81240799b6d57e1c
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2021-04-02 16:12:20 +0100

    created files for ha_client

--- ha_client/src/main.rs
+++ ha_client/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod verifying;
+
+use structopt::StructOpt;
+use color_eyre::eyre::Result;
+
+#[derive(StructOpt)]
+#[structopt(name = "HA_Client", about = "Checking on server satus")]
+struct Opt {
+
+    #[structopt(name = "server", long, default_value = "http://[::1]:50051")]
+    server_url : String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    println!("Starting HA_Client");
+
+    let opt = Opt::from_args();
+
+    // Read stdin for commands
+
+    Ok(())
 }
