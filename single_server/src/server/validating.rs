c668b4e771ab26cbd613b56eb9953b6486993bf8
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2021-04-02 11:58:19 +0100

    parsing server side dome

--- single_server/src/server/validating.rs
+++ single_server/src/server/validating.rs
@@ -1,5 +1,7 @@
 use color_eyre::eyre::Result;
 
+use std::convert::TryFrom;
+
 use tonic::{Request, Response, Status};
 
 use protos::location_storage::location_storage_server::{LocationStorage, LocationStorageServer};
@@ -9,13 +11,41 @@ use protos::location_storage::{SubmitLocationReportRequest, SubmitLocationReport
 #[derive(Default)]
 pub struct MyLocationStorage {}
 
+impl MyLocationStorage {
+    fn new() -> MyLocationStorage {
+        MyLocationStorage {}
+    }
+
+    fn parse_valid_idx(&self, idx : u32) -> Result<usize, Status> {
+        let idx = usize::try_from(idx);
+        if idx.is_err() /*|| !self.timeline.is_point(idx.unwrap())*/ {
+            return Err(Status::invalid_argument(format!("Not a valid id: {:}.", idx.unwrap())));
+        }
+        Ok(idx.unwrap())
+    }
+
+    fn parse_valid_epoch(&self, epoch : u32) -> Result<usize, Status> {
+        let epoch = usize::try_from(epoch);
+        if epoch.is_err() /*|| self.timeline.epochs() <= result_req_epoch.unwrap()*/ {
+            return Err(Status::invalid_argument(format!("Not a valid epoch: {:}.", epoch.unwrap())));
+        }
+        Ok(epoch.unwrap())
+    }
+}
+
 #[tonic::async_trait]
 impl LocationStorage for MyLocationStorage {
     async fn submit_location_report(
         &self,
         request: Request<SubmitLocationReportRequest>,
     ) -> Result<Response<SubmitLocationReportResponse>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
+        let request = request.get_ref();
+
+        let (req_idx, epoch) =
+            match (self.parse_valid_idx(request.idx), self.parse_valid_idx(request.epoch)) {
+                (Ok(idx), Ok(epoch)) => (idx, epoch),
+                (Err(err), _) | (_, Err(err)) => return Err(err),
+        };
 
         Ok(Response::new(SubmitLocationReportResponse {}))
     }
@@ -24,7 +54,13 @@ impl LocationStorage for MyLocationStorage {
         &self,
         request: Request<ObtainLocationReportRequest>,
     ) -> Result<Response<ObtainLocationReportResponse>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
+        let request = request.get_ref();
+
+        let (req_idx, epoch) =
+            match (self.parse_valid_idx(request.idx), self.parse_valid_epoch(request.epoch)) {
+                (Ok(idx), Ok(epoch)) => (idx, epoch),
+                (Err(err), _) | (_, Err(err)) => return Err(err),
+        };
 
         let reply = ObtainLocationReportResponse {
             report : Some(Report {proofs : vec![]}),
